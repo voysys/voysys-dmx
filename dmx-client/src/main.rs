7b0716e6f4798d3429642a41287c92d9fcb51782
Niclas Olmenius <niclas@voysys.se>
2023-12-01 20:29:06 +0100

    fix deps

--- dmx-client/src/main.rs
+++ dmx-client/src/main.rs
@@ -8,7 +8,10 @@ use ewebsock::{WsMessage, WsReceiver, WsSender};
 use serde::{Deserialize, Serialize};
 use std::fs;
 
+#[cfg(not(target_arch = "wasm32"))]
 use std::time::Instant;
+#[cfg(target_arch = "wasm32")]
+use web_time::Instant;
 
 mod channel;
 
@@ -219,7 +222,7 @@ impl eframe::App for App {
                 }
             }
 
-            res.buffer[100] = self.smoke.unwrap_or_default();
+            res.buffer[63] = self.smoke.unwrap_or_default();
 
             while let Some(_event) = self.ws_receiver.try_recv() {}
 
